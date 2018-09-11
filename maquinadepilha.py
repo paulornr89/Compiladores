b593b4e6207da15f0fea09040367d1232c4813ab
Paulo Roberto Nunes da Rosa <cpte.paulornr@gmail.com>
2018-09-11 17:32:07 -0300

    Trabalho da maquina de pilha

---
+++ maquinadepilha.py
@@ -0,0 +1,15 @@
+################      Trabalho de Compiladores: Máquina de Pilha       ################
+################         Nomes: Paulo Roberto, Yuri Weisshahn          ################
+
+###FUNÇÕES###
+#def push():
+
+#def pop():
+
+#def operacao():
+
+
+def main():
+	print("Teste")
+
+main()
